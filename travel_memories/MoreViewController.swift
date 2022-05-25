625431d236974de94c9c82f6230de0273f62af97
BhautikPethani <c0854487@mylambton.ca>
2022-05-25 12:03:33 -0400

    Detail screen's UI - Completed Image for video, Added Play button, Header, Subtitle, And description

---
+++ travel_memories/MoreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewMoreControllerViewController.swift
+//  travel_memories
+//
+//  Created by Bhautik Pethani on 2022-05-18.
+//
+
+import MediaPlayer
+import UIKit
+
+class MoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
