35b7fbfd4eb7bf89209cf85a41ea8cbf4af60175
BhautikPethani <c0854487@mylambton.ca>
2022-05-25 14:13:44 -0400

    Dynamic image collection view created

--- travel_memories/MoreViewController.swift
+++ travel_memories/MoreViewController.swift
@@ -8,23 +8,32 @@
 import MediaPlayer
 import UIKit
 
-class MoreViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class MoreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return subImages.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = imagesCollectionView.dequeueReusableCell(withReuseIdentifier: "PlaceImages", for: indexPath) as! SubImagesCollectionViewCell
+        cell.SubImage.image=UIImage(named: subImages[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:155, height: 145)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
+    var subImages = ["Tajmahal_1","Tajmahal_2","Tajmahal_3"];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagesCollectionView.delegate = self;
+        imagesCollectionView.dataSource = self;
     }
-    */
 
 }
