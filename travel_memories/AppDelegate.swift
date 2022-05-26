7c46c5211b357f48d92477b08bdc8a55ce81b5c8
BhautikPethani <c0854487@mylambton.ca>
2022-05-25 21:01:04 -0400

    Splash screen done

--- travel_memories/AppDelegate.swift
+++ travel_memories/AppDelegate.swift
@@ -14,12 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 2.0)
         
 //        let path = Bundle.main.path(forResource: "v1", ofType: "mp4");
 //        print(path);
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
