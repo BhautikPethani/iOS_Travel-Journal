c311750b1aedc6787a38ace187d221fc53676b8d
Aanchal <bansalaanchal96@gmail.com>
2022-05-31 14:57:06 -0400

    video thumbnail added to detail view controller

--- travel_memories/Controller/AddFormViewController.swift
+++ travel_memories/Controller/AddFormViewController.swift
@@ -177,7 +177,7 @@ class AddFormViewController: UIViewController, CLLocationManagerDelegate, MKMapV
         {
             finalMedia.append(UploadedMediaModel(originalUrl: videoFileURL!, mediaType: 1))
         }
-        placeModel = PlacesModel(id: "", name: placeName.text!, shortDescription: shortDescription.text!, latitude: Location[0], longitude: Location[1], media: finalMedia)
+        placeModel = PlacesModel(id: "", name: placeName.text!, shortDescription: shortDescription.text!, latitude: Location[0], longitude: Location[1], media: finalMedia, videoURL: videoFileLink)
         JournalDataManager.shared.saveNewPlace(place: placeModel!)
         
         performSegue(withIdentifier: "saved", sender: self)
@@ -247,7 +247,7 @@ extension AddFormViewController: UIImagePickerControllerDelegate, UINavigationCo
             self.media.append(media.lastPathComponent!)
             imagesCollectionView.isHidden = false
             self.imagesCollectionView.reloadData()
-            noMediaView.isHidden = true
+//            noMediaView.isHidden = true
         }
         dismiss(animated: true)
         
